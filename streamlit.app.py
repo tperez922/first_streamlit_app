c6effb43d1e75ca623bd7c9a0a0064a43624d7ea
tperez922 <taylor.a.perez@ibm.com>
2023-07-06 16:13:29 -0500

    Update streamlit.app.py
    typo

--- streamlit.app.py
+++ streamlit.app.py
@@ -9,7 +9,7 @@ streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
+my_fruit_list = my_fruit_list.set_index('Fruit')
 import pandas 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
@@ -20,4 +20,4 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado'
 
 # Display the table on the page.
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
+
