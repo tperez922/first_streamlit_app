448c9774f707efe112ffd7ac083109815661b4bc
tperez922 <taylor.a.perez@ibm.com>
2023-07-11 09:21:11 -0500

    Update streamlit.app.py
    updated indent line 31

--- streamlit.app.py
+++ streamlit.app.py
@@ -27,7 +27,8 @@ streamlit.dataframe(fruits_to_show)
 
 #New section to display fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
-try:fruit_choice = streamlit.text_input('What fruit would you like information about?')
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruity_choice:
     streamlit.error("Please select a fruit to get information")
   else: fruityvice_response= requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
