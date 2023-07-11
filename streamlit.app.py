722494a34fa5edb52dc34b6197dbea50de2d09c0
tperez922 <taylor.a.perez@ibm.com>
2023-07-11 10:40:19 -0500

    Update streamlit.app.py
    typo line 60

--- streamlit.app.py
+++ streamlit.app.py
@@ -57,7 +57,7 @@ streamlit.dataframe(fruityvice_normalized)
 
 streamlit.header("The fruit load list contains:")
 #snowflake-related functions
-def get+fruit_load_list():
+def get_fruit_load_list():
 with my_cnx_cursor() as my_cur:
   my_cur.execute("select * from fruit_load_list")
   return my_cur.fetchall()
